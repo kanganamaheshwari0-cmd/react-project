7410fd0ee94936599818b1eb90808ae3d3158143
Kangana Maheshwari <kanganamaheshwari0@gmail.com>
2026-09-06 14:35:52 +0530

    Inline CSS Styling in React.js

--- react-project/src/components/SeriesCard.jsx
+++ react-project/src/components/SeriesCard.jsx
@@ -1,23 +1,30 @@
   const SeriesCard = ({data}) => {
     // console.log(props)
-    const {id, img_url, name, rating, description, cast, genre, watch_url } = data;
+    const { img_url, name, rating, description, cast, genre, watch_url } = data;
+
+    const btn_style = {
+        padding: "0.8rem 1.6rem",
+        border:"none",
+        fontSize:"1.6rem",
+        backgroundColor:"var(--bnt-hover-bg-color)",
+        color:"var(--bg-color)"
+    }
     return(
-                <li>
+                <li className="card">
                     <div>
-                        <img src={img_url}
-                         alt={name}
-                         width="40%"
-                         height="40%"
-                         />
+                        <img src={img_url} alt={name} className="w-full h-104 object-cover" />
                     </div>
+
+                    <div className="card-content">
                     <h2>Name: {name}</h2>
                     <h3>Rating: {rating}</h3>
                     <p>Summary: {description}</p>
-                    <p>Genre: {genre}</p>
-                    <p>Cast: {cast}</p>
+                    <p>Genre: {genre.join(", ")}</p>
+                    <p>Cast: {cast.join(", ")}</p>
                     <a href={watch_url} target='blank'>
-                        <button>Watch Now</button>
+                        <button style={btn_style}>Watch Now</button>
                     </a>
+                    </div>
                 </li>
             );
 }
